ea6ce2212d52b870d68e978c8ed5d554f695f028
bloodlean <bloodlean7@gmail.com>
2023-10-01 11:57:23 +0300

    commit5

--- api/urls.py
+++ api/urls.py
@@ -3,11 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('study_center/', study_center),
-    path('study_detail/<int:pk>/', study_detail),
+    path('study_detail/<pk>/', study_detail),
     path('teacher/', teacher),
-    path('teacher_detail/<int:pk>/', teacher_detail),
+    path('teacher_detail/<pk>/', teacher_detail),
     path('student/', student),
-    path('student_detail/<int:pk>/', student_detail),
+    path('student_detail/<pk>/', student_detail),
 
     path('auth/', include('dj_rest_auth.urls'))
 ]

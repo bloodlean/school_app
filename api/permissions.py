8ca7ae1e043d2947c7dc55938ce847ade9bd768e
bloodlean <bloodlean7@gmail.com>
2023-10-02 17:16:58 +0300

    commit8

--- api/permissions.py
+++ api/permissions.py
@@ -4,62 +4,47 @@ from rest_framework.permissions import BasePermission
 #STUDY_CENTER PERMISSIONS
 
 class Study_centerPermission(BasePermission):
-
     def has_permission(self, request, view):
-        
         if request.method == 'GET':
             return True
+        elif request.method == 'POST':
+            return request.user.is_staff
 
 class StudyDetailPermission(BasePermission):
-
     def has_permission(self, request, view):
-        
         if request.method == 'GET':
             return True 
-
-        elif request.method in ['POST', 'PUT', 'DELETE']:
+        elif request.method in ['PUT', 'DELETE']:
             return request.user.is_staff
 
 #TEACHER PERMISSIONS
 
 class TeacherPermission(BasePermission):
-    
     def has_permission(self, request, view):
-        
         if request.method == 'GET':
             return True
-
         elif request.method == 'POST':
             return request.user.is_staff
 
 class TeacherDetailPermission(BasePermission):
-
     def has_permission(self, request, view):
-        
         if request.method == 'GET':
-            return True 
-
-        elif request.method in ['POST', 'PUT', 'DELETE']:
+            return True
+        elif request.method in ['PUT', 'DELETE']:
             return request.user.is_staff
 
 #STUDENT PERMISSIONS
 
 class StudentPermission(BasePermission):
-    
     def has_permission(self, request, view):
-        
         if request.method == 'GET':
             return request.user.is_staff
-
         elif request.method == 'POST':
             return request.user.is_staff
 
 class StudentDetailPermission(BasePermission):
-
     def has_permission(self, request, view):
-        
         if request.method == 'GET':
             return request.user.is_staff
-
-        elif request.method in ['POST', 'PUT', 'DELETE']:
+        elif request.method in ['PUT', 'DELETE']:
             return request.user.is_staff
